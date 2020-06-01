0e5bf2aabdfdd86f71f6da8526886fb60b7fe345
Marcus Abukari <marcus.abukari@nn.nl>
2020-06-01 17:22:53 +0200

    Remove tensorflow downgrade

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,40 +1,41 @@
 console.log("Running contentScript")
 
-let price_el = document.querySelector("#vip-ad-price-container > span:nth-child(1)");
+const price_el = document.querySelector("#vip-ad-price-container > span:nth-child(1)");
 const price = price_el.innerHTML
 price_el.innerHTML = "Berekenen...";
 
+const prediction_el = price_el.cloneNode(true);
+
 const auto = {}
-
 auto['upload_datum'] = document.querySelector("#displayed-since > span:nth-child(3)").innerHTML;
-var divs = document.querySelectorAll('#car-attributes > div.car-feature-table.spec-table.spec-table_flex > .spec-table-item'), i;
 
+let divs = document.querySelectorAll('#car-attributes > div.car-feature-table.spec-table.spec-table_flex > .spec-table-item'), i;
 divs.forEach(feature => {
     let key = feature.getElementsByClassName('key')[0].innerText;
-  let value = feature.getElementsByClassName('value')[0].innerText;
+    let value = feature.getElementsByClassName('value')[0].innerText;
 
-
-  if (key.includes("Kenteken")) {
-      auto['kenteken'] = value
-  }
-  if (key.includes("Brandstof")) {
-      auto['is_benzine'] = value.includes('Benzine')
-  }
-  if (key.includes("Transmissie")) {
-      auto['is_handgeschakeld'] = value.includes('Handmatig')
-  }
-  if (key.includes("Vermogen")) {
-      auto['vermogen'] = parseInt(value)
-  }
-  if (key.includes('Kilometerstand')) {
-      auto['kilometer_stand'] = parseInt(value.replace(/\D/g,''))
-  }
-  if (key.includes('Bouwjaar')) {
-      auto['bouwjaar'] = parseInt(value.replace(/\D/g,''))
-  }
+    if (key.includes("Kenteken")) {
+        auto['kenteken'] = value
+    }
+    if (key.includes("Brandstof")) {
+        auto['is_benzine'] = value.includes('Benzine')
+    }
+    if (key.includes("Transmissie")) {
+        auto['is_handgeschakeld'] = value.includes('Handmatig')
+    }
+    if (key.includes("Vermogen")) {
+        auto['vermogen'] = parseInt(value)
+    }
+    if (key.includes('Kilometerstand')) {
+        auto['kilometer_stand'] = parseInt(value.replace(/\D/g, ''))
+    }
+    if (key.includes('Bouwjaar')) {
+        auto['bouwjaar'] = parseInt(value.replace(/\D/g, ''))
+    }
 })
 
-fetch('http://127.0.0.1:8000/predict/', {
+// fetch('http://127.0.0.1:8000/predict/', {
+fetch('https://sheltered-chamber-82504.herokuapp.com/predict/', {
     method: 'post',
     body: JSON.stringify(auto)
 }).then(response => {
@@ -43,16 +44,13 @@ fetch('http://127.0.0.1:8000/predict/', {
     .then(prediction_obj => {
         const prediction = prediction_obj.prediction;
 
-        let price_el = document.querySelector("#vip-ad-price-container > span:nth-child(1)");
-        let prediction_el = price_el.cloneNode(true);
-
         prediction_el.innerHTML = '\u20AC ' + prediction + ',00'
 
         price_el.innerHTML = price
         prediction_el.innerHTML = " 🤖" + '👉 ' + prediction_el.innerHTML
 
 
-        if (prediction > parseInt(price.replace(/\D/g,'')) / 100) {
+        if (prediction > parseInt(price.replace(/\D/g, '')) / 100) {
             prediction_el.innerHTML = prediction_el.innerHTML + ' 🤑'
         } else {
             prediction_el.innerHTML = prediction_el.innerHTML + ' 😤'
@@ -64,4 +62,6 @@ fetch('http://127.0.0.1:8000/predict/', {
         }
 
         document.querySelector("#vip-ad-price-container").appendChild(prediction_el)
-    })
+    }).catch(response => {
+
+})
