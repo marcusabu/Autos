e5a442e36b5a35c8e2d87def8e4ec2b7e9c7bcd1
Marcus Abukari <marcusabu@gmail.com>
2020-06-12 21:18:40 +0200

    New scrape

--- Autos/settings.py
+++ Autos/settings.py
@@ -18,10 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p06ivpb-r#)7x8ox#x(lit)vwy$ln4jki20ms8^1(wxk8mr18x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
