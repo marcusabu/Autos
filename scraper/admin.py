c93d0c8a70ef5f8ef45ce63ca3088d5b3a8c8229
Marcus Abukari <marcusabu@gmail.com>
2020-06-30 22:13:08 +0200

    Cleanup

--- scraper/admin.py
+++ scraper/admin.py
@@ -4,7 +4,7 @@ from django.utils.html import format_html
 
 
 class AutoAdmin(admin.ModelAdmin):
-    list_display = [field.name if field.name is not 'url' else 'visit' for field in Auto._meta.get_fields() ]
+    list_display = [field.name if field.name is not 'url' else 'visit' for field in Auto._meta.get_fields()]
     list_filter = ('bouwjaar', 'is_handgeschakeld', 'is_benzine', 'bron')
     list_display_links = ('titel',)
 
